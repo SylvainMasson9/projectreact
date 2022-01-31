fb1dc01cbccd6a4d49addffd3d14f519f65bfd93
SylvainMasson9 <sylvain952@hotmail.com>
2022-01-31 14:58:15 +0100

    ajout button crotte zen

---
+++ src/ButtonPerso.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function ButtonPerso(test) {
+  return (
+    < button > {test.coucou} crottte de zen </button>
+  );
+}
+
+export default ButtonPerso;
